e5bbcf10cef315c03fdaf57923d23f9f3970d4b8
Diego Vilhalva <diegorocha450@yahoo.com>
2025-01-28 10:51:27 -0300

    navbar improvements added

--- src/segments/Navbar/index.jsx
+++ src/segments/Navbar/index.jsx
@@ -5,16 +5,32 @@ import { Link } from "react-scroll"
 import Socials from "../../components/Socials"
 import { CgMenuRight } from "react-icons/cg"
 import Sidebar from "../../components/Sidebar"
-import { useState } from "react"
+import { useMemo, useState } from "react"
+import { convertHexToRGBA } from "../../util"
 const Navbar = () => {
   const [open,setOpen] = useState(false)
+  const [drop,setDrop] = useState(false)
+
+  const dropNavbar = () => {
+    if (window.scrollY > 100) {
+      setDrop(true)
+    }else{
+      setDrop(false)
+    }
+  }
+
+  useMemo(() => {
+    window.addEventListener("scroll",dropNavbar)
+    return () => window.removeEventListener("scroll",dropNavbar)
+  })
+
   return (
     <>
       <Sidebar open={open} onClose={() => setOpen(!open)} />
         {open  && (
-          <div className="sidebar-overlay" onClick={() => setOpen(!open)} />
+          <div className="sidebar-overlay" onClick={() => setOpen(!open)} style={{background:convertHexToRGBA('--bg-base',0.8)}} />
         )}
-      <nav id="navbar">
+      <nav id="navbar" className={drop ? "blur drop":""}>
         <DataWaveLogo />
         <div className="route-wrapper">
           {navRoutes.map((route, i) => (
